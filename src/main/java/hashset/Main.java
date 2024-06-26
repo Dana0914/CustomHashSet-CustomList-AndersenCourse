6f7bfd60600221953d091b73acb2c3136e4eab7f
Dana <Dana.ackerman0914@gmail.com>
2024-06-26 16:54:18 +0500

    Created instances of CustomHashSet class and tested it in main function

--- src/main/java/hashset/Main.java
+++ src/main/java/hashset/Main.java
@@ -6,7 +6,16 @@ public class Main {
         System.out.println(set.add("A"));
         System.out.println(set.add("B"));
         System.out.println(set.add("C"));
+        System.out.println(set.add("W"));
         System.out.println(set.remove("A"));
         System.out.println(set.contains("B"));
+
+        CustomHashSet<Integer> integerSet = new CustomHashSet<>();
+        System.out.println(integerSet.add(4));
+        System.out.println(integerSet.add(5));
+        System.out.println(integerSet.add(6));
+        System.out.println(integerSet.remove(4));
+
+
     }
 }
