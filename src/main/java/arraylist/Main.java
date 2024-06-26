fad2cd8fc08bcb2cba029cfb619652b32ef761d5
Dana <Dana.ackerman0914@gmail.com>
2024-06-26 16:43:51 +0500

    Tested in main function

---
+++ src/main/java/arraylist/Main.java
@@ -0,0 +1,26 @@
+package arraylist;
+
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new List();
+        list.add(0, "ggb");
+        list.add(1, "ydg");
+        list.add(2, "gn");
+        list.add(3, "fv");
+        list.add(4, "fgbf");
+        list.add(5, "fgg");
+        list.add(6, "ggg");
+        list.add(7, "ggg");
+        list.add(8, "ggg");
+        list.add(9, "ggg");
+        list.add(10, "ggg");
+        System.out.println(list.get(4));
+        System.out.println(list.remove(10));
+        System.out.println(list.size());
+
+
+
+
+    }
+}
