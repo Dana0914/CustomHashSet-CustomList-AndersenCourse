e2cfd76b77b3ef86e240cc8e4d6053ac04478b6a
Dana <Dana.ackerman0914@gmail.com>
2024-06-27 00:25:56 +0500

    Added Node class with data and next variables

---
+++ src/main/java/hashset/Node.java
@@ -0,0 +1,25 @@
+package hashset;
+
+public class Node<T> {
+    T data;
+    Node<T> next;
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
