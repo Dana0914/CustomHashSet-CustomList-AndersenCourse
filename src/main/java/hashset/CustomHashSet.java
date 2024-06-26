0600b756d2450899c6b0591808f9e1786d4158ad
Dana <Dana.ackerman0914@gmail.com>
2024-06-27 00:25:23 +0500

    Second Custom Implementation of CustomHashSet

--- src/main/java/hashset/CustomHashSet.java
+++ src/main/java/hashset/CustomHashSet.java
@@ -1,60 +1,85 @@
 package hashset;
 
 
-@SuppressWarnings("unchecked")
 public class CustomHashSet<T> implements ISet<T> {
-    private T[] buckets;
-    private int size;
+    private final static int DEFAULT_INITIAL_CAPACITY = 10;
+    Node[] nodes;
+    int size;
 
     public CustomHashSet() {
-        buckets = (T[]) new Object[10];
+        nodes = new Node[DEFAULT_INITIAL_CAPACITY];
         size = 0;
+    }
+    public int getHash(T element) {
+        return element.hashCode() % nodes.length;
     }
     @Override
     public boolean add(T element) {
-        if (size == buckets.length) {
-            resize();
-        }
-        if (contains(element)) {
+        if (element == null) {
             return false;
         }
-        buckets[size++] = element;
+        if (size == nodes.length) {
+            resize();
+        }
+        int bucketIndex = getHash(element);
+        Node current = nodes[bucketIndex];
+        while (current != null) {
+            if (current.data.equals(element)) {
+                return false;
+            }
+            current = current.next;
+        }
+        // if current node is empty then add element
+        Node newNode = new Node(element);
+        nodes[bucketIndex] = newNode;
+        // if current node is not empty then add to next
+        nodes[bucketIndex].next = newNode;
+        size++;
         return true;
     }
 
     @Override
     public boolean contains(T element) {
-        for (int i = 0; i < size; i++) {
-            if (buckets[i].equals(element)) {
+        if (element == null) {
+            return false;
+        }
+        int bucketIndex = getHash(element);
+        Node current = nodes[bucketIndex];
+        while (current != null) {
+            if (current.data.equals(element)) {
                 return true;
             }
-
+            current = current.next;
         }
         return false;
     }
 
     @Override
-    public boolean remove(T element) {
-        for (int i = 0; i < size; i++) {
-            if (buckets[i].equals(element)) {
-                buckets[i] = buckets[i+1];
-                return true;
+    public void remove(T element) {
+        int bucketIndex = getHash(element);
+        Node current = nodes[bucketIndex];
+        while (current != null) {
+            if (current.data.equals(element)) {
+                nodes[bucketIndex] = null;
+                return;
             }
+            current = current.next;
         }
-        return false;
+        size--;
+
     }
 
     @Override
     public int size() {
         return size;
     }
-
     private void resize() {
-        int newSize = (int) ((size * 1.5) + 1);
-        T[] newBuckets = (T[]) new Object[newSize];
-        if (size >= 0) System.arraycopy(buckets, 0, newBuckets, 0, size);
-        buckets = newBuckets;
+        int newCapacity = (int) ((DEFAULT_INITIAL_CAPACITY * 1.5) + 1);
+        Node[] newNodes = new Node[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newNodes[i] = nodes[i];
+        }
+        nodes = newNodes;
     }
-
 }
 
