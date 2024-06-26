825e51c1b6789b9c30a2935e4bac9342217ad850
Dana <Dana.ackerman0914@gmail.com>
2024-06-27 00:26:13 +0500

    Tested in main function

--- src/main/java/hashset/Main.java
+++ src/main/java/hashset/Main.java
@@ -5,16 +5,11 @@ public class Main {
         CustomHashSet<String> set = new CustomHashSet<>();
         System.out.println(set.add("A"));
         System.out.println(set.add("B"));
-        System.out.println(set.add("C"));
-        System.out.println(set.add("W"));
-        System.out.println(set.remove("A"));
-        System.out.println(set.contains("B"));
+        System.out.println(set.add("A"));
+        set.remove("A");
+        System.out.println(set.contains("A"));
 
-        CustomHashSet<Integer> integerSet = new CustomHashSet<>();
-        System.out.println(integerSet.add(4));
-        System.out.println(integerSet.add(5));
-        System.out.println(integerSet.add(6));
-        System.out.println(integerSet.remove(4));
+
 
 
     }
